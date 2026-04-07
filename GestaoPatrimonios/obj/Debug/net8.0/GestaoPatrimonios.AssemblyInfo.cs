98b99000197003f1501fb860e80684f94751d54d
Mayara Almeida <mayara.s.almeida8@aluno.senai.br>
2026-04-06 23:00:26 -0300

    :white_check_mark: Testes da camada de tipo de alteração finalizados

--- GestaoPatrimonios/obj/Debug/net8.0/GestaoPatrimonios.AssemblyInfo.cs
+++ GestaoPatrimonios/obj/Debug/net8.0/GestaoPatrimonios.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GestaoPatrimonios")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+31951140c779435d55e48ae36dfef6f8970652f5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a149146c761a1e06b2456ee136f1baaec790e3ca")]
 [assembly: System.Reflection.AssemblyProductAttribute("GestaoPatrimonios")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GestaoPatrimonios")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
